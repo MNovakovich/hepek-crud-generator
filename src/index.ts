6e31e83df19f3249138721b27841dd57128f65f7
markonovakovics7 <marko.novakovic@s7designcreative.com>
2021-11-02 08:57:30 +0100

    formated file

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,34 @@
 #!/usr/bin/env node
 // index.ts
-import { Command, InvalidArgumentError} from 'commander';
-import { GenerateModels } from './commands';
-import { runNpmCommand } from './utils'
-var inquirer = require('inquirer');
+import { Command, InvalidArgumentError } from "commander";
+import { GenerateModels } from "./commands";
+import { runNpmCommand } from "./utils";
+var inquirer = require("inquirer");
 
-
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const program = new Command();
-program.version('0.0.1');
+program.version("0.0.1");
 
 const generateModels = new GenerateModels(program, inquirer);
 
 generateModels.create((answers) => {
   //  execSync('npm install moment', { stdio: 'inherit', cwd: 'path/to/dir' });
-     if(answers.framework === 'nestjs') {
-        if(answers.next_crud) {
-            runNpmCommand('npm i @nestjsx/crud @nestjsx/crud-request @nestjsx/crud-typeorm');
-        }
-          console.log('create next crud')
-     } else if( answers.framework === 'expressjs' ) {
-        console.log('create expressjs')
-     }
+  if (answers.framework === "nestjs") {
+    if (answers.next_crud) {
+      runNpmCommand(
+        "npm i @nestjsx/crud @nestjsx/crud-request @nestjsx/crud-typeorm"
+      );
+    }
+    console.log("create next crud");
+  } else if (answers.framework === "expressjs") {
+    console.log("create expressjs");
+  }
+
+  console.log(answers);
 });
 
-
-
-program.parse(process.argv)
+program.parse(process.argv);
 
 // function myParseInt(value, dummyPrevious) {
 //     // parseInt takes a string and a radix
@@ -37,19 +38,19 @@ program.parse(process.argv)
 //     }
 //     return parsedValue;
 //   }
-  
+
 //   function increaseVerbosity(dummyValue, previous) {
 //     return previous + 1;
 //   }
-  
+
 //   function collect(value, previous) {
 //     return previous.concat([value]);
 //   }
-  
+
 //   function commaSeparatedList(value, dummyPrevious) {
 //     return value.split(',');
 //   }
-  
+
 //   program
 //     .option('-f, --float <number>', 'float argument', parseFloat)
 //     .option('-i, --integer <number>', 'integer argument', myParseInt)
@@ -57,13 +58,12 @@ program.parse(process.argv)
 //     .option('-c, --collect <value>', 'repeatable value', collect, [])
 //     .option('-l, --list <items>', 'comma separated list', commaSeparatedList)
 //   ;
-  
+
 //   program.parse();
-  
+
 //   const options = program.opts();
 //   if (options.float !== undefined) console.log(`float: ${options.float}`);
 //   if (options.integer !== undefined) console.log(`integer: ${options.integer}`);
 //   if (options.verbose > 0) console.log(`verbosity: ${options.verbose}`);
 //   if (options.collect.length > 0) console.log(options.collect);
 //   if (options.list !== undefined) console.log(options.list);
-
